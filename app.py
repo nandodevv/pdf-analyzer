f5327ecd8c27cad8165976a4e7747a5c386f2e9c
Fernando <frodriguezdavila@icloud.com>
2026-04-07 18:49:20 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,15 @@
-from openai import OpenAI  #→ importas solo la clase OpenAI de dentro de la librería
+from openai import OpenAI  
 
 import streamlit as st
-import PyPDF2   #→ importas toda la libreria 
+import PyPDF2   
+from dotenv import load_dotenv  
+import os 
 
-from dotenv import load_dotenv #dotenv es una libreria , 
-#su unica funcion es leer el archivo env y leer las claves que hay dentro
-import os #os es una libreria que viene con python y te permite acceder a cosas
-#del sistema operativo: como las variables de entorno
 
 
 
 load_dotenv()
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
-#Exacto. client es tu conexión a GPT. Es una variable que llama a GPT
-# Cada vez que quieras preguntarle algo lo harás a través de client.
 
 
 
@@ -43,15 +39,7 @@ if archivo is not None:
 
 
 
-            #1. if pregunta= Si existe una pregunta del usuario. pregunta =input del usuario
-            # 2. respuesta(variable) = llamamos a gpt con client y 
-            # la funcion chat completions.create 
-            # 2.1 Especificamos el modelo
-            #. 2.2 Messages (lista de dos mensajes) asi es como gpt entiende una
-            #conversacion: 2 roles
-            # a) system, content: Le dices como debe comprotarse (instrucciones)
-            # b) user , content : es el input del usuario que le das tu a la ia
-            #junto con el texto para que responda 
+         
 
         
 
